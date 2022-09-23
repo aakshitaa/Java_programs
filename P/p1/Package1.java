124ddaa98369cbcc0957f0f07a2ff62d5b1f5e9a
aakshitaa <aakshitasingh786@gmail.com>
2022-09-23 21:55:28 +0530

    This programs contains the implementation of concepts like Interface, Inheritance, Abstraction, Packages.

--- P/p1/Package1.java
+++ P/p1/Package1.java
@@ -12,16 +12,17 @@ public class Package1 {
     public static class C1 {
         Scanner sc = new Scanner(System.in);
         private long acc_number = 11244570;
-        protected int salary = 25000;
+        public static int salary = 25000;
         public String name = "Aakshita Singh";
         int empid = 78472;
+        protected int acc_bal = 758444;
 
         public void bankdetails() {
-            System.out.println("want to check account number? Y/N");
+            System.out.println("want to check account details? Y/N");
             String ch = sc.nextLine();
             switch (ch) {
                 case "Y":
-                    System.out.println("Account number is: " + acc_number);
+                    System.out.println("Account number is: " + acc_number + "\n Account Balance is: " + acc_bal);
                     break;
                 case "N":
                     System.out.println("Ok!");
